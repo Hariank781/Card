ed7d953feb8e97153b70e5766e28aa0d4da46f6f
Hariank <hariankd_cs20.rvitm@rvei.edu.in>
2023-04-09 18:56:09 +0530

    Final Commit

---
+++ app/src/main/java/com/example/newcard/MainActivity2.java
@@ -0,0 +1,25 @@
+package com.example.newcard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main2);
+    }
+
+    public void bback(View view) {
+        Intent intent=new Intent(this,MainActivity.class);
+        startActivity(intent);
+    }
+}
